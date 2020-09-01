0c4d69f0dcf0042a34374faf038d7433fb83f875
Jas4118 <70446078+Jas4118@users.noreply.github.com>
2020-09-01 10:50:08 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World"); // Outputs out Hello World.
 
 	}
 
